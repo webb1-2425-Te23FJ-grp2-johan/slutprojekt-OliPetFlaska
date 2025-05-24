01d3267de8cb7fc8113c2af1e7b5fddf796941f4
OliPetFlaska <olipet049@edu.linkoping.se>
2025-05-24 18:12:58 +0000

    aaaaaaaaaaaaaaaaaaaa

--- java/main.js
+++ java/main.js
@@ -1,15 +1,7 @@
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const navMenu = document.getElementById("nav-menu");
-const navLinks = navMenu.querySelectorAll("a");
 
 hamburgerBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show");
   navMenu.classList.toggle("hidden");
-});
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    navMenu.classList.remove("show");
-    navMenu.classList.add("hidden");
-  });
 });
